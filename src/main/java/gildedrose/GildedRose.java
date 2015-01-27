32f21bf7338e7c5e6db1464df2d3def37ea66730
Steffen Eichenberg <Steffen.Eichenberg@lindenbaum.eu>
2015-01-27 08:55:42 +0100

    Extract method objects

--- src/main/java/gildedrose/GildedRose.java
+++ src/main/java/gildedrose/GildedRose.java
@@ -12,52 +12,100 @@ class GildedRose {
 
 			Item item = items[i];
 			if (item.name == "Aged Brie") {
+				new AgedBrie(item).invoke();
+      }
+      else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+				new BackstagePass(item).invoke();
+      }
+      else if (item.name == "Sulfuras, Hand of Ragnaros") {
+				new Sulfuras(item).invoke();
+			}
+      else {
+				new RegularItem(item).invoke();
+      }
+    }
+  }
+
+	private class AgedBrie {
+		private Item item;
+
+		public AgedBrie(Item item) {
+			this.item = item;
+		}
+
+		public void invoke() {
+			if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+			item.sellIn = item.sellIn - 1;
+			if (item.sellIn < 0) {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
         }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
+      }
+		}
+	}
+
+	private class BackstagePass {
+		private Item item;
+
+		public BackstagePass(Item item) {
+			this.item = item;
+		}
+
+		public void invoke() {
+			if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+
+        if (item.sellIn < 6) {
           if (item.quality < 50) {
             item.quality = item.quality + 1;
           }
         }
       }
-      else if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-          item.quality = item.quality - item.quality;
-        }
+			item.sellIn = item.sellIn - 1;
+			if (item.sellIn < 0) {
+        item.quality = item.quality - item.quality;
       }
-      else if (item.name == "Sulfuras, Hand of Ragnaros") {
-        if (item.quality > 0) {
-        }
+		}
+	}
+
+	private class Sulfuras {
+		private Item item;
+
+		public Sulfuras(Item item) {
+			this.item = item;
+		}
+
+		public void invoke() {
+			if (item.quality > 0) {
       }
-      else {
+		}
+	}
+
+	private class RegularItem {
+		private Item item;
+
+		public RegularItem(Item item) {
+			this.item = item;
+		}
+
+		public void invoke() {
+			if (item.quality > 0) {
+        item.quality = item.quality - 1;
+      }
+			item.sellIn = item.sellIn - 1;
+			if (item.sellIn < 0) {
         if (item.quality > 0) {
           item.quality = item.quality - 1;
         }
-        item.sellIn = item.sellIn - 1;
-        if (item.sellIn < 0) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
-        }
       }
-    }
-  }
+		}
+	}
 }
